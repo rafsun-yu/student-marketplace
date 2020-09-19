2b547fa27df55f942e4c3dfd07f7d26cc2709cf2
KhalidBrinto <khalid.shafiq@northsouth.edu>
2020-09-19 14:34:08 +0600

    Frontend v1.211

--- Projects/js/cart.js
+++ Projects/js/cart.js
@@ -1,3 +1,49 @@
-document.getElementById("addcart").addEventListener("click", function(){
-    document.getElementById("count").innerText = "Hello world";
-});
+var k = 0;
+
+// adding data to local storage
+const addtoCartBtn = document.getElementsByClassName("addcart");
+let items = [];
+
+for (let i = 0; i < addtoCartBtn.length; i++) {
+  addtoCartBtn[i].addEventListener("click", function (e) {
+
+    if (typeof Storage !== "undefined") {
+      let item = {
+        id: i + 1,
+        name: e.target.parentElement.children[1].textContent,
+        price: e.target.parentElement.children[2].children[0].textContent,
+        no: 1,
+      };
+
+      if (JSON.parse(localStorage.getItem("items") === null)) {
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
+        window.location.reload();
+      } else {
+        const localItem = JSON.parse(localStorage.getItem("items"));
+        localItem.map(data =>{
+            if(item.id == data.id){
+                item.no = data.no + 1;
+                console.log(item);
+            }else{
+                items.push(data);
+            }      
+        });
+
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
+        window.location.reload();
+      }
+    } else {
+      console.log("storage is not working");
+    }
+  });
+
+  const itemCount = document.querySelector('#count');
+  let no = 0;
+  JSON.parse(localStorage.getItem('items')).map(data=>{
+      no = no+data.no;
+  });
+  itemCount.innerHTML = no;
+
+}
--- src/sql/admin-approve-item.sql
+++
@@ -1,5 +0,0 @@
--- Gets items which hasn't been approved yet with seller information
-SELECT i.id, s.id, s.name, title, description, category, image_url, price 
-FROM items i 
-    INNER JOIN sellers s ON (i.seller_id = s.id)
-WHERE approver IS NULL;
