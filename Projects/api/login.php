f6f857902bcf085df0d8541882392a37af404a6b
KhalidBrinto <khalid.shafiq@northsouth.edu>
2020-09-30 01:00:44 +0600

    updated

--- Projects/api/login.php
+++ Projects/api/login.php
@@ -1,19 +1,31 @@
 <?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+$user_id = $_POST['username'];
+$password = $_POST['password'];
 
-    $connect = mysqli_connect('localhost','root','','student_marketplace');
-    $sql = "SELECT username, email, password FROM login WHERE username = '$username' AND password = '$password';";
-    $result = mysqli_query($connect,$sql);
-    if(mysqli_num_rows($result)==1){
-        if(!isset($_COOKIE['name'])){
-            setcookie('name', $username, time()+3600);
-        }        
+$connect = mysqli_connect('localhost', 'root', '', 'student_marketplace');
+$sql = "SELECT user_id, password, CASE WHEN user_id IN(SELECT id FROM sellers) THEN 'seller' WHEN user_id IN(SELECT id FROM buyers) THEN 'buyer' ELSE 'admin' END AS user_type FROM login WHERE user_id = {$user_id} AND password = '{$password}';";
+$result = mysqli_query($connect, $sql);
+$array = array();
+$row = mysqli_fetch_assoc($result);
+$array[] = $row;
+if($array[0]['user_type'] == 'admin'){
+    echo 'Redirecting to admin page';
+}
+if ($array[0]['user_type'] == 'seller') {
+    echo 'Redirecting to seller page';
+}
+if ($array[0]['user_type'] == 'buyer') {
+    if (mysqli_num_rows($result) == 1) {
+        if (!isset($_COOKIE['name'])) {
+            setcookie('name', $user_id, time() + 3600);
+        }
         session_start();
-        $_SESSION['name'] = $username;
+        $_SESSION['name'] = $user_id;
         header('Location: https://localhost/Projects/index.php');
-    }else{
+    } else {
         echo "Username or password incorrect";
     }
-    
+}
+
+mysqli_close($connect);
 ?>
