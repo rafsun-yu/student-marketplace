60272ff6d77dca52b5880424f66fa1e4f1bf7cce
rafsun-nsu <rafsun.prince@northsouth.edu>
2020-09-29 11:01:03 +0600

    Added search options in admin-approved-items

--- api/admin-approved-items.php
+++ api/admin-approved-items.php
@@ -5,6 +5,9 @@
     $database = new Database();
     $conn = $database->getConnection();
 
+
+    //AND title LIKE '%%' AND s.id = ''
+
     $sql = "SELECT i.id as item_id, title, s.id as seller_id, name as seller_name, 
                 CASE 
                     WHEN featurer IS NOT NULL THEN 'true'
@@ -12,6 +15,18 @@
                 END as featured 
             FROM items i INNER JOIN users s ON (i.seller_id = s.id)
             WHERE i.approver IS NOT NULL";
+
+    if(isset($_GET['title'])) {
+        $title = $_GET['title'];
+        if ($title != "")
+            $sql .= " AND title LIKE '%" . $title . "%'";
+    }
+
+    if(isset($_GET['sellerId'])) {
+        $sellerId = $_GET['sellerId'];
+        if ($sellerId != "")
+            $sql .= " AND s.id = '" . $sellerId . "'";
+    }
 
     $result = $conn->query($sql);
 
--- src/app/lib/util.js
+++ src/app/lib/util.js
@@ -12,6 +12,15 @@ function httpCall(url, callback) {
     xhttp.send();
 }
 
+function getUrlParam(name, url) {
+    if (!url) url = location.href;
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(url);
+    return results == null ? null : results[1];
+}
+
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
